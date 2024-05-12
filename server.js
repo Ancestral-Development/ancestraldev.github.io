f48ffda24dfdbf101337833d33a552e5c63bbdfb
Cizek612 <88971139+Cizek612@users.noreply.github.com>
2024-05-12 23:15:46 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+function scrollToTeam() {
+  var element = document.querySelector('.mobile-android-development');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToServices() {
+  var element = document.querySelector('.services');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToAbout() {
+  var element = document.querySelector('.about-us');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToWork() {
+  var element = document.querySelector('.our-team-is-container');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
